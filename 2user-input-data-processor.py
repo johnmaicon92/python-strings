8650543175e70b3e623d002af28ccf864ef077af
johnmaicon92 <johnmaicon_sempre@hotmail.com>
2024-05-09 20:58:50 -0400

    Preparing to start a new assignment

---
+++ 2user-input-data-processor.py
@@ -0,0 +1,10 @@
+"""
+Task 1: Input Length Validator
+Write a script that checks the length of the user's first name and last name. Both should be at least 2 characters long. If not, print an error message.
+
+Task 2: Password Complexity Checker
+Create a function that checks the complexity of a user's password. The password must be at least 8 characters long, contain one uppercase letter, one lowercase letter, and one number. If the password does not meet these criteria, print a message explaining the complexity requirements.
+
+Task 3: Email Formatter
+Implement a script that ensures all user email addresses are in a standard format
+"""
